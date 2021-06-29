82fced568dece920795a03be55de9c211332d625
Mohsin Ali <55811423+mosin92@users.noreply.github.com>
2021-06-29 12:06:09 +0500

    update files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world! from Nouman Malik</div>
+  return <div>Hello world! from Nouman Malik and Mohsin Malik</div>
 }
